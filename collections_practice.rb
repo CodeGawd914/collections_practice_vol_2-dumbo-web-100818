a59c7754259dd2772baa0019fb58f073edc61a98
Travis Desir <travisgdesir31@gmail.com>
2018-09-24 21:59:51 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? { |thing| thing.start_with?("r")}
+end
+
+def contain_a(element)
+  element.select { |item| item.include?("a")}
+end
+
+def first_wa(array)
+  array.find { |item| item.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.delete_if {|item| item != item.to_s}
+end
+
+def count_elements(array)
+  count = []
+  array.uniq.each do |item| 
+    item[:count] = array.count(item) 
+    count.push(item)
+  end
+count
+end
+
+def merge_data(keys,data)
+  array = []
+  keys.each do |thing|
+    name = thing[:first_name]
+    data.each do |x|
+      if x[name]
+        mergedPerson = x[name]
+        mergedPerson[:first_name] = name
+        array << mergedPerson
+      end
+    end
+  end
+  array
+end
+
+def find_cool(array)
+ array.select {|item| item.values.include?("cool") ? item : nil}
+end
+
+def organize_schools(hash)
+  haxh = {}
+   hash.each do |key, value|
+    location = value[:location]
+    if haxh[location]
+        haxh[location] << key
+    else
+      haxh[location] = []
+      haxh[location] << key
+    end
+  end
+  haxh
+end
